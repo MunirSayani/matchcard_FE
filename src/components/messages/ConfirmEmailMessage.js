5a1c40f6c1f99035d613c00f7b733b5c98ade280
code-io <mmsayani@ucdavis.edu>
2018-07-01 15:29:59 -0700

    authorized routes

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+const componentName = () => (
+  <Message info>
+    <Message.Header> Please, verify your email address </Message.Header>
+  </Message>
+);
+
+export default componentName;
